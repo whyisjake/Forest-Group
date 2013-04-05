c7852763c101fe3786a1fc4e71546c553cc740ab
Jake Spurlock <whyisjake@gmail.com>
2013-04-05 15:51:43 -0700

    Hooking in a few more functions.

--- functions.php
+++ functions.php
@@ -10,7 +10,23 @@
  
 function fg_enqueue_scripts() {
 	wp_enqueue_script( 'bootstrap', get_stylesheet_directory_uri() . '/js/bootstrap.js', array( 'jquery' ), false, true );
+	wp_enqueue_script( 'skycons', get_stylesheet_directory_uri() . '/_inc/skycons/skycons.js' );
 	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.css' );
 }
 
 add_action( 'wp_enqueue_scripts', 'fg_enqueue_scripts' );
+
+function make_action_after_setup_theme() {
+
+	add_theme_support('post-thumbnails' );
+	//add_image_size( 'comment-thumb', 70, 70, true );
+	
+	// Content Width
+	if ( ! isset( $content_width ) )
+		$content_width = 620;
+
+	// Custom Backgrounds
+	add_theme_support( 'custom-background' );
+	add_theme_support( 'automatic-feed-links' );
+}
+add_action( 'after_setup_theme', 'make_action_after_setup_theme' );
