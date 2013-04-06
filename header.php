dc5fa0b270340c90ad5971cd314ac97279426f3f
Jake Spurlock <whyisjake@gmail.com>
2013-04-06 10:12:18 -0700

    Start of this, how about Saturday...

--- header.php
+++ header.php
@@ -33,6 +33,12 @@
 		
 	<header>
 		
-		
+		<div class="container">
+			
+			<h1>
+				<a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' ); ?></a>
+			</h1>
+			
+		</div>
 		
 	</header>
