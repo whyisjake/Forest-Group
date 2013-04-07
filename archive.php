6535cbd14683097a01b2f677c22bb3f00750e469
Jake Spurlock <whyisjake@gmail.com>
2013-04-06 17:01:22 -0700

    Linking up the title

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@ get_header(); ?>
 			
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 					
-					<?php the_title( '<h1>', '</h1>' ); ?>
+					<?php the_title( '<h1><a href="' . the_permalink() . '">', '</a></h1>' ); ?>
 						
 					<?php the_excerpt(); ?>
 					
