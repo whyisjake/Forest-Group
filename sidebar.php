288b99331a1dbb7a25a545860eddcd64329bc3a7
Jake Spurlock <whyisjake@gmail.com>
2013-04-06 17:39:23 -0700

    Sidebar.

--- sidebar.php
+++ sidebar.php
@@ -4,12 +4,37 @@
 		
 		<div class="inner">
 		
-			<ul class="nav nav-stacked">
+			<h3>Resources</h3>
+			
+			<ul class="nav">
 				
-				<li><a href="#">Test</a></li>
-				<li><a href="#">Test</a></li>
-				<li><a href="#">Test</a></li>
-				<li><a href="#">Test</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#fire">Fire Safety</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#forests">Forests</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#funding">Funding</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#watershed">Watersheds</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#roads">Roads</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#maps">Maps</a></li>
+				<li><a href="http://forestconservation.iamawimp.org/resources/#conference">Conference</a></li>
+				
+			</ul>
+			
+		</div>
+		
+	</div>
+	
+	<div class="widget">
+		
+		<div class="inner">
+		
+			<h3>What We Do</h3>
+			
+			<ul class="nav">
+				
+				<li><a href="http://forestconservation.iamawimp.org/what-we-do/#bag">Brown Bag Lecture Series</li>
+				<li><a href="http://forestconservation.iamawimp.org/what-we-do/#forest">Forest Stewardship Workshops</li>
+				<li><a href="http://forestconservation.iamawimp.org/what-we-do/#house">House Meetings</li>
+				<li><a href="http://forestconservation.iamawimp.org/what-we-do/#monthly">Monthly Meetings</li>
+				<li><a href="http://forestconservation.iamawimp.org/what-we-do/#conf">Forest Conservation Conference</li>
 				
 			</ul>
 			
